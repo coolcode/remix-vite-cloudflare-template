301594281b5e2d84ce547598256693fa2d9c7414
coolcode <coolcode@live.com>
2024-02-14 17:31:37 +0800

    🥑 @remix/node installGlobals

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,11 @@ import {
   unstable_cloudflarePreset as cloudflare,
   unstable_vitePlugin as remix,
 } from "@remix-run/dev"
+import { installGlobals } from "@remix-run/node"
 import { defineConfig } from "vite"
 import tsconfigPaths from "vite-tsconfig-paths"
+
+installGlobals()
 
 export default defineConfig({
   plugins: [
